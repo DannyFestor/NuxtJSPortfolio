37b1e7433e3bb01fc6475c7fc332578b60c1dc14
Danny Festor <danny.festor@gmail.com>
2020-04-28 09:42:18 +0900

    Fix Container Height

---
+++ pages/contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container">
+    <section>CONTACT</section>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+    
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container">
+    <section>PROJECTS</section>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+    
+  }
+})
+</script>
+
+<style>
+
+</style>

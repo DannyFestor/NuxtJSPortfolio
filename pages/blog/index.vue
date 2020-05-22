3ec714a54ebaddbffa00130de80cfebef9e096ee
Danny Festor <danny.festor@gmail.com>
2020-05-22 11:24:45 +0900

    Fix link bug

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,7 +3,7 @@
     <h1>My blog posts:</h1>
     <ul>
       <li v-for="post in blogPosts" :key="post.slug">
-        <nuxt-link :to="'blog/' + post.slug">{{ post.slug }}</nuxt-link>
+        <nuxt-link :to="post.slug">{{ post.slug }}</nuxt-link>
       </li>
     </ul>
   </div>
@@ -14,16 +14,6 @@ export default {
     blogPosts() {
       return this.$store.state.blogPosts;
     }
-  },
-  mounted() {
-    console.log(this.$store.state.blogPosts);
-   }
-  // async asyncData() {
-  //   const resolve = await require.context('~/content/', true, /\.md$/);
-  //   const imports = resolve.keys().map((key) => {
-  //     return resolve(key);
-  //   });
-  //   return { posts: imports };
-  // },
-}
+  }
+};
 </script>
